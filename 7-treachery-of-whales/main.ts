45b27af2955ba0d4b4f3955242054160e7e68ddc
Will Warner <will.r.warner@gmail.com>
2022-01-01 21:26:38 -0600

    add part two logging

--- 7-treachery-of-whales/main.ts
+++ 7-treachery-of-whales/main.ts
@@ -1,13 +1,18 @@
 import {
   calculateMinFuelForAlignmentWithConstantFuelUsage,
+  calculateMinFuelForAlignmentWithVariableFuelUsage,
   FILE_NAME,
   readInCrabPositions,
 } from "./lib.ts";
 
 const crabPositions = await readInCrabPositions(FILE_NAME);
-const minFuelForAlignment =
+const minFuelForAlignmentPartOne =
   calculateMinFuelForAlignmentWithConstantFuelUsage(crabPositions);
 
+const minFuelForAlignmentPartTwo =
+  calculateMinFuelForAlignmentWithVariableFuelUsage(crabPositions);
+
 console.log("");
-console.log("Part One: Min Fuel For Alignment = " + minFuelForAlignment);
+console.log("Part One: Min Fuel For Alignment = " + minFuelForAlignmentPartOne);
+console.log("Part TWo: Min Fuel For Alignment = " + minFuelForAlignmentPartTwo);
 console.log("");
