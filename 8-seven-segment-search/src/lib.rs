8c8a62d9456a210b83ca7d4d808e79360ceb45e4
Will Warner <will.warner@nerdery.com>
2022-01-03 14:14:38 -0600

    add part one solution

--- 8-seven-segment-search/src/lib.rs
+++ 8-seven-segment-search/src/lib.rs
@@ -1,15 +1,62 @@
 use std::fs;
 
-pub fn read_in_digits(filename: &str) -> String {
-    fs::read_to_string(filename).expect("Something went wrong reading the file")
+pub const FILE_NAME: &str = "data.txt";
+pub const FILE_NAME_EXAMPLE: &str = "data-example.txt";
+
+pub struct Display {
+    pub unique_signals: Vec<String>,
+    pub outputs: Vec<String>,
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+pub fn read_in_displays(filename: &str) -> Vec<Display> {
+    let input_str = fs::read_to_string(filename).expect("error reading file");
 
-//     #[test]
-//     fn internal() {
-//         assert_eq!(4, read_in_digits("temp"));
-//     }
-// }
+    input_str
+        .lines()
+        .map(|line| {
+            let (signals, displays) = line.split_once(" | ").unwrap();
+            let unique_signals = signals.split(" ").map(|x| x.to_owned()).collect();
+            let outputs = displays.split(" ").map(|x| x.to_owned()).collect();
+
+            Display {
+                unique_signals,
+                outputs,
+            }
+        })
+        .collect()
+}
+
+pub fn calculate_digit_count_of_1_4_7_8(displays: Vec<Display>) -> u64 {
+    let mut digit_count = 0;
+    for display in displays {
+        for output in display.outputs {
+            let increment;
+
+            match output.len() {
+                2..=4 | 7 => increment = 1,
+                _ => increment = 0,
+            }
+
+            digit_count = digit_count + increment;
+        }
+    }
+
+    digit_count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_pass_part_one_for_example() {
+        let displays = read_in_displays(FILE_NAME_EXAMPLE);
+        assert_eq!(26, calculate_digit_count_of_1_4_7_8(displays));
+    }
+
+    #[test]
+    fn should_pass_part_one() {
+        let displays = read_in_displays(FILE_NAME);
+        assert_eq!(255, calculate_digit_count_of_1_4_7_8(displays));
+    }
+}
--- 8-seven-segment-search/src/main.rs
+++ 8-seven-segment-search/src/main.rs
@@ -1,14 +1,14 @@
 mod lib;
 
 fn main() {
+    let displays = lib::read_in_displays(lib::FILE_NAME);
+    let digit_count_part_one = lib::calculate_digit_count_of_1_4_7_8(displays);
+
     println!("");
-
     println!(
-        "Part One: Num 1, 4, 7, or 8 Digits = {}",
-        lib::read_in_digits("data-example.txt")
+        "Part One: Count of 1, 4, 7, or 8 Digits = {}",
+        digit_count_part_one
     );
-
     println!("Part Two: ?? = {}", "??");
-
     println!("");
 }
