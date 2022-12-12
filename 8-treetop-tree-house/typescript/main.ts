1ad83f361d06e735dfd3f1b7da9e74c591f2563d
Will Warner <will.warner@nerdery.com>
2022-12-11 22:12:12 -0600

    add day 8 part two solution

--- 8-treetop-tree-house/typescript/main.ts
+++ 8-treetop-tree-house/typescript/main.ts
@@ -8,34 +8,33 @@ main();
 function main(): void {
   console.log("Part One Example:", partOne(DATA_FILE_NAME_EXAMPLE));
   console.log("Part One        :", partOne(DATA_FILE_NAME));
-  // console.log("Part Two Example One  :", partTwo(DATA_FILE_NAME_EXAMPLE));
-  // console.log("Part Two              :", partTwo(DATA_FILE_NAME));
+  console.log("Part Two Example:", partTwo(DATA_FILE_NAME_EXAMPLE));
+  console.log("Part Two        :", partTwo(DATA_FILE_NAME));
 }
 
 type Tree = {
   row: number;
   col: number;
   height: number;
-  isVisible: boolean;
+  isVisible?: boolean;
+  scenicScore?: number;
 };
 
 function partOne(filename: string): number {
-  const data = Deno.readTextFileSync(filename);
-  const trees = data
-    .split("\n")
-    .map((row) => row.split(""))
-    .map((row, rowIndex) =>
-      row.map((tree, colIndex) => toTree(tree, rowIndex, colIndex))
-    )
-    .map((row, _index, rawTrees) =>
-      row.map((tree) => updateTreeVisibility(tree, rawTrees))
-    );
+  const trees = readInTrees(filename).map((row, _index, rawTrees) =>
+    row.map((tree) => updateTreeVisibility(tree, rawTrees))
+  );
 
   return countNumVisibleTrees(trees);
 }
 
-// function partTwo(filename: string): number {
-// }
+function partTwo(filename: string): number {
+  const trees = readInTrees(filename).map((row, _index, rawTrees) =>
+    row.map((tree) => updateScenicScore(tree, rawTrees))
+  );
+
+  return calculateMaxScenicScore(trees);
+}
 
 function countNumVisibleTrees(trees: Tree[][]): number {
   return trees.flat(1).reduce((count, curTree) => {
@@ -44,12 +43,34 @@ function countNumVisibleTrees(trees: Tree[][]): number {
   }, 0);
 }
 
+function calculateMaxScenicScore(trees: Tree[][]): number {
+  return trees.flat(1).reduce((maxScore, curTree) => {
+    if (curTree.scenicScore) {
+      if (curTree.scenicScore > maxScore) {
+        maxScore = curTree.scenicScore;
+      }
+    }
+
+    return maxScore;
+  }, 0);
+}
+
+function readInTrees(filename: string): Tree[][] {
+  const data = Deno.readTextFileSync(filename);
+  const trees = data
+    .split("\n")
+    .map((row) => row.split(""))
+    .map((row, rowIndex) =>
+      row.map((tree, colIndex) => toTree(tree, rowIndex, colIndex))
+    );
+  return trees;
+}
+
 function toTree(treeHeight: string, rowIndex: number, colIndex: number): Tree {
   return {
     row: rowIndex,
     col: colIndex,
     height: Number(treeHeight),
-    isVisible: false,
   };
 }
 
@@ -93,6 +114,28 @@ function calculateVisibility(tree: Tree, trees: Tree[][]): boolean {
   return isVisible;
 }
 
+function updateScenicScore(tree: Tree, trees: Tree[][]): Tree {
+  const row = trees[tree.row];
+  const col = flatOne(trees).filter((x) => x.col === tree.col);
+
+  const upTrees = col.slice(0, tree.row);
+  const downTrees = col.slice(tree.row + 1);
+  const leftTrees = row.slice(0, tree.col);
+  const rightTrees = row.slice(tree.col + 1);
+
+  let upScore = upTrees.reverse().findIndex((x) => x.height >= tree.height);
+  let downScore = downTrees.findIndex((x) => x.height >= tree.height);
+  let leftScore = leftTrees.reverse().findIndex((x) => x.height >= tree.height);
+  let rightScore = rightTrees.findIndex((x) => x.height >= tree.height);
+
+  upScore = upScore === -1 ? upTrees.length : upScore + 1;
+  downScore = downScore === -1 ? downTrees.length : downScore + 1;
+  leftScore = leftScore === -1 ? leftTrees.length : leftScore + 1;
+  rightScore = rightScore === -1 ? rightTrees.length : rightScore + 1;
+
+  return { ...tree, scenicScore: upScore * downScore * leftScore * rightScore };
+}
+
 Deno.test(function shouldPassPartOneExample() {
   assertEquals(partOne(DATA_FILE_NAME_EXAMPLE), 21);
 });
@@ -101,10 +144,10 @@ Deno.test(function shouldPassPartOne() {
   assertEquals(partOne(DATA_FILE_NAME), 1823);
 });
 
-// Deno.test(function shouldPassPartTwoExample() {
-//   assertEquals(partTwo(DATA_FILE_NAME_EXAMPLE), 24933642);
-// });
+Deno.test(function shouldPassPartTwoExample() {
+  assertEquals(partTwo(DATA_FILE_NAME_EXAMPLE), 8);
+});
 
-// Deno.test(function shouldPassPartTwo() {
-//   assertEquals(partTwo(DATA_FILE_NAME), 1498966);
-// });
+Deno.test(function shouldPassPartTwo() {
+  assertEquals(partTwo(DATA_FILE_NAME), 211680);
+});
