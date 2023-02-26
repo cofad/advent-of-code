78591b48d3b16c8c7fd585b3e09fb8ae86987358
Will Warner <will.warner@nerdery.com>
2023-02-25 20:16:41 -0600

    add day 10 part 2 solution

--- 10-cathode-ray-tube/rust/src/main.rs
+++ 10-cathode-ray-tube/rust/src/main.rs
@@ -19,48 +19,22 @@ struct Instruction {
 struct Cycle {
     count: i32,
     register: i32,
+    crt: i32,
 }
 
 fn main() {
     println!("Part One - Example: {}", part_one(DATA_FILE_NAME_EXAMPLE));
     println!("Part One          : {}", part_one(DATA_FILE_NAME));
+    println!(
+        "Part Two - Example: \n\n{}",
+        part_two(DATA_FILE_NAME_EXAMPLE)
+    );
+    println!("Part Two          : \n\n{}", part_two(DATA_FILE_NAME));
 }
 
 fn part_one(filename: &str) -> i32 {
-    let mut register = 1;
-    let mut count = 1;
-    let mut cycles: Vec<Cycle> = vec![];
-
     let instructions = parse_data(filename);
-
-    for instruction in instructions {
-        match instruction.opcode {
-            Opcode::ADDX => {
-                count += 1;
-
-                cycles.push(Cycle {
-                    count: count,
-                    register: register,
-                });
-
-                count += 1;
-
-                match instruction.value {
-                    Some(x) => register += x,
-                    None => register += 0,
-                };
-            }
-            Opcode::NOOP => {
-                count += 1;
-            }
-        }
-
-        cycles.push(Cycle {
-            count: count.to_owned(),
-            register: register.to_owned(),
-        });
-    }
-
+    let cycles = create_cycles(instructions);
     let check_cycles = [20, 60, 100, 140, 180, 220];
 
     cycles
@@ -68,6 +42,85 @@ fn part_one(filename: &str) -> i32 {
         .filter(|x| check_cycles.contains(&x.count))
         .map(|x| x.register * x.count)
         .sum::<i32>()
+}
+
+fn part_two(filename: &str) -> String {
+    let instructions = parse_data(filename);
+    let cycles = create_cycles(instructions);
+    let crt = generate_crt(&cycles);
+    crt
+}
+
+fn create_cycles(instructions: Vec<Instruction>) -> Vec<Cycle> {
+    let mut register = 1;
+    let mut count = 1;
+    let mut cycles: Vec<Cycle> = vec![];
+
+    cycles.push(Cycle {
+        count,
+        register,
+        crt: (count - 1) % 40,
+    });
+
+    for instruction in instructions {
+        if count >= 240 {
+            break;
+        }
+
+        match instruction.opcode {
+            Opcode::ADDX => {
+                count += 1;
+
+                cycles.push(Cycle {
+                    count,
+                    register,
+                    crt: (count - 1) % 40,
+                });
+
+                match instruction.value {
+                    Some(x) => register += x,
+                    None => register += 0,
+                };
+
+                count += 1;
+
+                cycles.push(Cycle {
+                    count,
+                    register,
+                    crt: (count - 1) % 40,
+                });
+            }
+            Opcode::NOOP => {
+                count += 1;
+
+                cycles.push(Cycle {
+                    count,
+                    register,
+                    crt: (count - 1) % 40,
+                });
+            }
+        }
+    }
+
+    cycles
+}
+
+fn generate_crt(cycles: &Vec<Cycle>) -> String {
+    let mut crt = "".to_owned();
+
+    for cycle in cycles {
+        if (cycle.register - cycle.crt).abs() < 2 {
+            crt.push_str("#");
+        } else {
+            crt.push_str(".");
+        }
+
+        if cycle.crt != 0 && cycle.crt % 39 == 0 {
+            crt.push_str("\n");
+        }
+    }
+
+    crt
 }
 
 fn parse_data(filename: &str) -> Vec<Instruction> {
@@ -106,13 +159,29 @@ mod tests {
         assert_eq!(16880, part_one(DATA_FILE_NAME));
     }
 
-    // #[test]
-    // fn should_pass_part_two_example() {
-    //     assert_eq!(45000, part_two());
-    // }
+    #[test]
+    fn should_pass_part_two_example() {
+        let mut crt = String::from("");
+        crt.push_str("##..##..##..##..##..##..##..##..##..##..\n");
+        crt.push_str("###...###...###...###...###...###...###.\n");
+        crt.push_str("####....####....####....####....####....\n");
+        crt.push_str("#####.....#####.....#####.....#####.....\n");
+        crt.push_str("######......######......######......####\n");
+        crt.push_str("#######.......#######.......#######.....\n");
 
-    // #[test]
-    // fn should_pass_part_two() {
-    //     assert_eq!(45000, part_two());
-    // }
+        assert_eq!(crt, part_two(DATA_FILE_NAME_EXAMPLE));
+    }
+
+    #[test]
+    fn should_pass_part_two() {
+        let mut crt = String::from("");
+        crt.push_str("###..#..#..##..####..##....##.###..###..\n");
+        crt.push_str("#..#.#.#..#..#....#.#..#....#.#..#.#..#.\n");
+        crt.push_str("#..#.##...#..#...#..#..#....#.###..#..#.\n");
+        crt.push_str("###..#.#..####..#...####....#.#..#.###..\n");
+        crt.push_str("#.#..#.#..#..#.#....#..#.#..#.#..#.#.#..\n");
+        crt.push_str("#..#.#..#.#..#.####.#..#..##..###..#..#.\n");
+
+        assert_eq!(crt, part_two(DATA_FILE_NAME));
+    }
 }
