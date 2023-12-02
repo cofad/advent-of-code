970967af10f05e376edcd4d0b2cb0e6e17c34713
Will Warner <will.warner@nerdery.com>
2023-12-02 03:30:17 -0600

    feat: add day 2 solution

---
+++ day-two/main.test.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from "https://deno.land/std@0.200.0/assert/mod.ts";
+import { partOne, partTwo } from "./main.ts";
+
+const DATA_FILE_NAME = "./day-two/data.txt";
+const DATA_PART_ONE_EXAMPLE_FILE_NAME = "./day-two/data-part-one-example.txt";
+const DATA_PART_TWO_EXAMPLE_FILE_NAME = "./day-two/data-part-two-example.txt";
+
+Deno.test("Part One Example", () => {
+  const data = Deno.readTextFileSync(DATA_PART_ONE_EXAMPLE_FILE_NAME);
+  const cubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  assertEquals(partOne(data, cubes), 8);
+});
+
+Deno.test("Part One", () => {
+  const data = Deno.readTextFileSync(DATA_FILE_NAME);
+  const cubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  assertEquals(partOne(data, cubes), 2006);
+});
+
+Deno.test("Part Two Example", () => {
+  const data = Deno.readTextFileSync(DATA_PART_TWO_EXAMPLE_FILE_NAME);
+  assertEquals(partTwo(data), 2286);
+});
+
+Deno.test("Part Two", () => {
+  const data = Deno.readTextFileSync(DATA_FILE_NAME);
+  assertEquals(partTwo(data), 84911);
+});
