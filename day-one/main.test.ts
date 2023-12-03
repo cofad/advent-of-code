f2a74eca57ff45f2a5f9e202dfbd31ba52caff28
Will Warner <will.warner@nerdery.com>
2023-12-03 10:13:05 -0600

    chore: make file paths more generic

--- day-one/main.test.ts
+++ day-one/main.test.ts
@@ -1,9 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.200.0/assert/mod.ts";
 import { partOne, partTwo } from "./main.ts";
 
-const DATA_FILE_NAME = "./day-one/data.txt";
-const DATA_PART_ONE_EXAMPLE_FILE_NAME = "./day-one/data-part-one-example.txt";
-const DATA_PART_TWO_EXAMPLE_FILE_NAME = "./day-one/data-part-two-example.txt";
+const DATA_FILE_NAME = "./data.txt";
+const DATA_PART_ONE_EXAMPLE_FILE_NAME = "./data-part-one-example.txt";
+const DATA_PART_TWO_EXAMPLE_FILE_NAME = "./data-part-two-example.txt";
 
 Deno.test("Part One Example", () => {
   const data = Deno.readTextFileSync(DATA_PART_ONE_EXAMPLE_FILE_NAME);
